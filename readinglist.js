1d5a1f0a3f086f19f2fcf8b1681cf25616f31c62
Praew <114122183+Haluethaichanok-J@users.noreply.github.com>
2023-03-21 16:58:43 +0700

    Fixed

--- readinglist.js
+++ readinglist.js
@@ -1,23 +1,51 @@
-// const gJavascript = document.querySelector('#reading-list li:nth-child(2) .name');
-// console.log(gJavascript);
-// gJavascript.innerHTML = 'Advanced JAVASCRIPT!!!';
-
-
-
-
-
-// 
-
-
-// const readingList = document.querySelectorAll('#reading-list li .name');
-// readingList.forEach(function(list){
-//     list.textContent += 'book title: ' ;
-// }
-// )
-
-
-
-const readingList = document.querySelectorAll('#reading-list li .name');
-readingList.forEach(function(list){
-    list.textContent = 'book title: ' + list.textContent;
+//better way for element.addEventListener()
+const list = document.querySelector('#reading-list ul');
+list.addEventListener('click', function(e){
+    if (e.target.className == 'delete'){
+        const li = e.target.parentNode;
+        list.removeChild(li);
+     }
 })
+
+// //Interacting with Form -- add readinglist
+const addForm = document.forms['add-list'];
+addForm.addEventListener('submit',function(e){
+    e.preventDefault(); // prevent from refresh
+    const value = addForm.querySelector('input[type="text"]').value; //add new reading list in the text box
+   // console.log(value);
+
+// //create element
+ const li = document.createElement('li');
+
+ li.innerHTML = addForm;
+ list.appendChild(li);
+  
+ 
+ const newName = document.createElement('span');
+ // //add content
+ newName.textContent =value;
+ li.appendChild(newName);
+
+  const deleteBut = document.createElement('span');
+
+  // //add content
+  deleteBut.textContent = 'delete';
+  li.appendChild(deleteBut);
+
+
+  // //add classes
+newName.classList.add('name');
+deleteBut.classList.add('delete');
+
+// //append to DOM
+
+document.getElementsByTagName("ul").appendChild(li);
+document.getElementsByTagName("li").appendChild(newName);
+document.getElementsByTagName("li").appendChild(deleteBut);
+
+
+// li.appendChild(newName);
+// li.appendChild(deleteBut);
+// list.appendChild(li);
+
+});
